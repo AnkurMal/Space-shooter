df407aec6052b4192ba2f484aa04d1d80e55a43a
Ankur Mallick <ankurmallick80@gmail.com>
2024-07-18 09:29:54 +0530

    replaced audio.dll and its fucntions with macroquad's own audio functions

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=dylib=audio");
-    println!("cargo:rustc-link-search=native=./lib");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+use macroquad::audio::{load_sound, play_sound_once};
 use macroquad::prelude::*;
 
 use player::Player;
 mod player;
-mod sound;
-use sound::*;
 
 ///text_params!(font, size, color)
 macro_rules! text_params {
@@ -35,12 +34,11 @@ async fn main() {
     let screen_width = screen_width();
     let _screen_height = screen_height();
 
-    let audio = AudioDevice::new();
     let mut player_proj = Vec::new();
 
     set_pc_assets_folder("assets");
 
-    let sound = Sound::load(&audio, "assets/sound/gun.mp3");
+    let sound = load_sound("sound/gun.ogg").await.unwrap();
     let font = load_ttf_font("font/edge.ttf").await.unwrap();
     let laser = load_texture("images/png/laserGreen.png").await.unwrap();
     let mut player = Player::new(load_texture("images/png/player.png").await.unwrap(),
@@ -52,7 +50,7 @@ async fn main() {
         player.update(5.);
         if is_key_pressed(KeyCode::Space) {
             player_proj.push(Projectile::new(player.x+player.width()/2., player.y));
-            sound.play();
+            play_sound_once(&sound);
         }
 
         clear_background(BLACK);
